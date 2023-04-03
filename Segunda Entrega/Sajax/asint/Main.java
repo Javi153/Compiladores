f7cb049cddecd2560f6ce940195f008e4c028f99
savicente2109 <savice01@ucm.es>
2023-04-03 11:10:05 +0200

    Cambiado el Main.java (SajaxLex en lugar de Tiny) y una errata del cup.

--- Segunda Entrega/Sajax/asint/Main.java
+++ Segunda Entrega/Sajax/asint/Main.java
@@ -3,13 +3,13 @@ package asint;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import alex.AnalizadorLexicoTiny;
+import sajax_lex.AnalizadorLexicoSajax;
 
 public class Main {
    public static void main(String[] args) throws Exception {
      Reader input = new InputStreamReader(new FileInputStream(args[0]));
-	 AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
-	 AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(alex);
+	 AnalizadorLexicoSajax alex = new AnalizadorLexicoSajax(input);
+	 AnalizadorSintacticoSajax asint = new AnalizadorSintacticoSajax(alex);
 	 asint.parse();
  }
 }   
