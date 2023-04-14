85dbeeafc5c09e620f2092920f1f5a7c4661bf68
Javi153 <javijimenez153@gmail.com>
2023-04-14 17:30:37 +0200

    Esta es la buena

--- Segunda Entrega/Sajax/ast/KindE.java
+++ Segunda Entrega/Sajax/ast/KindE.java
@@ -4,7 +4,7 @@ public enum KindE {
   SUMA("suma"),RESTA("resta"),MUL("mult"),DIV("div"),MOD("mod"),POT("pot"),ENT("ent"),REAL("real"),
   IDEN("iden"),ID("igualdad"),DISTINTO("desigualdad"),OR("or"),AND("and"),NOT("not"), MENOR("menor"),
   MAYOR("mayor"),MENIGUAL("menorOIgual"), MAYIGUAL("mayorOIgual"),TRUE("true"),FALSE("false"), CALLFUN("llamadaFuncion"),
-  PUNTO("accesoACampo"), FLECHA("accesoADireccion"), ASTERISCO("asterisco"), CORCHETES("corchetes");
+  PUNTO("accesoACampo"), FLECHA("accesoADireccion"), ASTERISCO("asterisco"), CORCHETES("accesoAPosicion");
 
   private String name;
   KindE(String s){
