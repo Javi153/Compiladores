eb5903a3ac45b9280ea1a4b3165befc342dd1ac8
savicente2109 <savice01@ucm.es>
2023-04-25 17:43:55 +0200

    cambio en compila.bash para que ponga input.txt

--- Segunda Entrega/Sajax/compila.bash
+++ Segunda Entrega/Sajax/compila.bash
@@ -6,4 +6,4 @@ cd constructorast/
 java -cp cup.jar java_cup.Main -parser ConstructorASTExp -symbols ClaseLexica -nopositions ConstructorAST.cup
 cd ..
 javac -cp "cup.jar:." */*.java
-java -cp "cup.jar:." constructorast.Main input_error.txt
+java -cp "cup.jar:." constructorast.Main input.txt
