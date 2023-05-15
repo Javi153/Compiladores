5bc56a869646b549efa4f1e597963c71980b9bd4
savicente2109 <savice01@ucm.es>
2023-05-15 12:31:14 +0200

    más errores

--- Ultima Entrega/Sajax/compila.bash
+++ Ultima Entrega/Sajax/compila.bash
@@ -6,6 +6,6 @@ cd constructorast/
 java -cp cup.jar java_cup.Main -parser ConstructorASTExp -symbols ClaseLexica -nopositions ConstructorAST.cup
 cd ..
 javac -cp "cup.jar:." */*.java
-java -cp "cup.jar:." constructorast.Main input3.txt
+java -cp "cup.jar:." constructorast.Main input.txt
 ./wat2wasm result.wat
 node SAJAX.js
